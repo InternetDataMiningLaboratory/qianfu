3cf8c3dd62dc48f2393e43a5a79efc38f4804e75
Jimin Huang <windworship2@163.com>
2016-02-29 21:10:52 +0800

    增加了LastDayPrice及对应css

--- handler/info.py
+++ handler/info.py
@@ -10,7 +10,16 @@ class InfoHandler(base.BaseHandler):
         The handler of the info page.
     '''
     def get(self, stock_id):
-        stock_item = ['1', '000001', u'平安银行', '89']
+        stock_item = [
+            '1',
+            '000001',
+            u'平安银行',
+            '89',
+            '12.84',
+            '1.17',
+            '+10.03%',
+            '商业银行-贸易',
+        ]
 
         # Render the shown page
         self.render(
