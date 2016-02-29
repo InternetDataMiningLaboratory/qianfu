b4fd2ee1cbc20d652b561d93f79a2d617d134b68
Jimin Huang <windworship2@163.com>
2016-02-29 20:46:34 +0800

    增加了BasicInfo与对应css并修复了handler的bug

--- handler/info.py
+++ handler/info.py
@@ -9,13 +9,13 @@ class InfoHandler(base.BaseHandler):
     '''
         The handler of the info page.
     '''
-    def get(self):
-        stock_item = ['1', '000001', '平安银行', '89']
+    def get(self, stock_id):
+        stock_item = ['1', '000001', u'平安银行', '89']
 
         # Render the shown page
         self.render(
             'info.html',
-            page_title=u'乾阜-'+stock_item[1]+'-'+stock_items[2],
+            page_title=u'乾阜-'+stock_item[1]+'-'+stock_item[2],
             css_file_name='info',
             stock_item=stock_item,
         )
